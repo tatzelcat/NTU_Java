d4121673ee42722f55209420fcb9c132b5c9233d
tatzelcat <trisha.fwt@gmail.com>
2025-07-14 20:57:28 +0800

    Updated for assignment 2.9 + 2.10

--- simplecrm/simple-crm/src/main/java/sg/ntu/simple_crm/CustomerController.java
+++ simplecrm/simple-crm/src/main/java/sg/ntu/simple_crm/CustomerController.java
@@ -3,15 +3,18 @@ package sg.ntu.simple_crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.*;
 
 @RestController
 public class CustomerController {
 
+    //Logger
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
-    // Pre-defined customer data
+    //Hashmap for storing customers
     private Map<String, Customer> customers = new HashMap<>();
     
     public CustomerController() {
@@ -23,12 +26,23 @@ public class CustomerController {
     //======= Customer Data Endpoint ===========
     @GetMapping("/customer/{customerId}")
     public ResponseEntity<?> getCustomer(@PathVariable String customerId) {
+        logger.info("GET request recieved for customer ID: {}", customerId);
         if (customers.containsKey(customerId)) {
+            logger.info("Customer found :D");
             return ResponseEntity.ok(customers.get(customerId));
         } else {
+            logger.info("Customer not found D:");
             return ResponseEntity.ok("No customer found");
         }
     }
+
+    @PostMapping("/customer")
+    public ResponseEntity<?> createCustomer(@RequestBody Customer customer) {
+
+    customers.put(customer.getID(), customer);
+        logger.info("Customer created successfully with ID: {}", customer.getID());
+        return ResponseEntity.status(201).body(customer);
+}
 }
 
 
